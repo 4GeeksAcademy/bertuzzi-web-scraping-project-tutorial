2f7439c5eabffa3845cf9f7a289884c244f7fc7f
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2025-02-01 08:30:39 +0000

    update project

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ import sqlite3
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 
 url = 'https://www.alphaquery.com/stock/TSLA/earnings-history'
 
